532c7c9d2148a5e275d5d5ef45d45eddddcd26c6
ALI NAWAB RANA <alinawabrana@gmail.com>
2024-08-03 17:00:02 +0500

    Creating Forget Password Screen & Resend Password Screen
    After creating the Email verification & Success Screen we will now create a Forget Password Screen.
    
    In this Screen, we have a Texts, TextFormField to enter the email at which the reset password link will be send & finally we have the elevated button to submit the email.
    
    After submitting the emial, we will be redirected to the reset password screen, where we will have a text showing that password has been sent and contains two buttons: One with the text done and other with the text resend email.
    
    NOTE: We also have a cross icon at the appbar of both screens BUT the cross Icon at reset password screen will redirect us to login screen instead of the forget passord screen because there we have use the Get.off instead of Get.to.
    Also, we have resue the same screen for reset password which we used for success screen but with one extra button in it.

--- lib/features/authentication/screens/login/widgets/login_form.dart
+++ lib/features/authentication/screens/login/widgets/login_form.dart
@@ -1,3 +1,4 @@
+import 'package:e_commerce_app/features/authentication/screens/password_configuration/forget_password.dart';
 import 'package:e_commerce_app/features/authentication/screens/signup/signup.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -58,7 +59,7 @@ class TLoginForm extends StatelessWidget {
 
                 /// Forget Password
                 TextButton(
-                  onPressed: () {},
+                  onPressed: () => Get.to(() => const ForgetPassword()),
                   child: const Text(TTexts.forgetPassword),
                 ),
               ],
---
+++ lib/features/authentication/screens/password_configuration/forget_password.dart
@@ -0,0 +1,61 @@
+import 'package:e_commerce_app/features/authentication/screens/password_configuration/reset_password.dart';
+import 'package:e_commerce_app/utils/constants/sizes.dart';
+import 'package:e_commerce_app/utils/constants/text_strings.dart';
+import 'package:e_commerce_app/utils/helpers/helper_functions.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:iconsax/iconsax.dart';
+
+class ForgetPassword extends StatelessWidget {
+  const ForgetPassword({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.all(TSizes.defaultSpace),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            /// Headings
+            Text(
+              TTexts.forgetPassword,
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            const SizedBox(
+              height: TSizes.spaceBtwItems,
+            ),
+            Text(
+              TTexts.forgetPasswordSubTitle,
+              style: Theme.of(context).textTheme.labelMedium,
+            ),
+            const SizedBox(
+              height: TSizes.spaceBtwSections * 2,
+            ),
+
+            /// Text Fields
+            TextFormField(
+              decoration: const InputDecoration(
+                  labelText: TTexts.email,
+                  prefixIcon: Icon(Iconsax.direct_right)),
+            ),
+            const SizedBox(
+              height: TSizes.spaceBtwSections,
+            ),
+
+            /// Submit Button
+            SizedBox(
+              width: THelperFunctions.screenWidth(),
+              child: ElevatedButton(
+                  //Now if we don't want to get to the previous screen and we want to jump back to the screen before this then we will use Get.off.
+                  // This will not save the navigation of this screen but will have the navigation to its previous screen
+                  onPressed: () => Get.off(() => const ResetPassword()),
+                  child: const Text(TTexts.submit)),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
