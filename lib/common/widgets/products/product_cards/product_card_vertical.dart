3c4a05b45b47d0c44c2884f05cfe59cda25f05b2
ALI NAWAB RANA <alinawabrana@gmail.com>
2024-08-18 11:59:23 +0500

    Creating the image slider of Product Details Screen
    Whenever someone click on the product from the home page, then they will be redirected to the Product Detail screen.
    
    Initially I have created the  Custom Container having the AppBar with Favourite Icon, a large size image at the ceter of the contaner and the image slider at the bottom of the container.
    
    Below this container, I have created Rating and share button, the discount badge with the discounted plus original value, title, stock status & the brand name with the icon.
    
    Next up we are gonnna use the attributes value to provide different color scheme of the product, Size, checkout button etc

--- lib/common/widgets/products/product_cards/product_card_vertical.dart
+++ lib/common/widgets/products/product_cards/product_card_vertical.dart
@@ -1,10 +1,12 @@
 import 'package:e_commerce_app/common/widgets/custom_shapes/containers/rounded_container.dart';
 import 'package:e_commerce_app/common/widgets/images/t_rounded_image.dart';
+import 'package:e_commerce_app/features/shop/screens/product_details/product_details.dart';
 import 'package:e_commerce_app/utils/constants/colors.dart';
 import 'package:e_commerce_app/utils/constants/image_strings.dart';
 import 'package:e_commerce_app/utils/constants/sizes.dart';
 import 'package:e_commerce_app/utils/helpers/helper_functions.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
 
 import '../../../styles/shadows.dart';
@@ -22,7 +24,7 @@ class TProductCardVertical extends StatelessWidget {
 
     /// Container with side paddings, color, edges, radius and shadows
     return GestureDetector(
-      onTap: () {},
+      onTap: () => Get.to(() => const ProductDetails()),
       child: Container(
         width: 180,
         padding: const EdgeInsets.all(1),
---
+++ lib/features/shop/screens/product_details/product_details.dart
@@ -0,0 +1,48 @@
+import 'package:e_commerce_app/features/shop/screens/product_details/widgets/product_detail_image_slider.dart';
+import 'package:e_commerce_app/features/shop/screens/product_details/widgets/product_meta_data.dart';
+import 'package:e_commerce_app/features/shop/screens/product_details/widgets/rating_share_widget.dart';
+import 'package:e_commerce_app/utils/constants/sizes.dart';
+import 'package:e_commerce_app/utils/helpers/helper_functions.dart';
+import 'package:flutter/material.dart';
+
+class ProductDetails extends StatelessWidget {
+  const ProductDetails({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final darkMode = THelperFunctions.isDarkMode(context);
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            /// 1 - Product Image Slider
+            TProductImageSlider(),
+
+            /// 2 - Poduct details
+            Padding(
+              padding: EdgeInsets.only(
+                  right: TSizes.defaultSpace,
+                  left: TSizes.defaultSpace,
+                  bottom: TSizes.defaultSpace),
+              child: Column(
+                children: [
+                  /// - Rating & Share Button
+
+                  TRatingAndShare(),
+
+                  /// - Price, Title, Stock & Brand
+                  TProductMetaData(),
+
+                  /// -- Attributes
+                  /// -- Checkout Button
+                  /// -- Description
+                  /// -- Reviews
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
